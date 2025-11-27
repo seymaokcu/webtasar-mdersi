30c9d81d967aab3ae34fe90767936fa4f0e5b6fc
seymaokcu <okcuseyma65@gmail.com>
2025-11-27 14:03:15 +0300

    Refactor mouse movement animation code
    Removed comments and adjusted animation effect.

--- üçüncühafta/mouseeffect/main.js
+++ üçüncühafta/mouseeffect/main.js
@@ -4,13 +4,11 @@ window.addEventListener("mousemove", function (e) {
     const pozX = e.clientX;
     const pozY = e.clientY;
 
-    // Bayrağı doğrudan fare konumuna ayarla
     flagCursor.style.left = `${pozX}px`;
     flagCursor.style.top = `${pozY}px`;
 
-    // Animasyon efekti için
     flagCursor.animate({
         left: `${pozX}px`,
         top: `${pozY}px`
-    }, { duration: 300, fill: "forwards" }); // Animasyon süresi 300ms, daha hızlı tepki için
+    }, { duration: 300, fill: "forwards" }); 
 });
